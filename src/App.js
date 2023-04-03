d512c93c07144fd4ca2c712c150b3ee854debe3f
Kevin Bargary <kevinbargary@gmail.com>
2023-04-03 19:08:21 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+/* import logo from "./main.svg"; */
 import "./App.css";
-
-function Foo() {
-  return <div>Placeholder</div>;
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Foo />
+          <h1>Coming soon..</h1>
         </header>
       </div>
     );
