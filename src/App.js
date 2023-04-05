c2874da09c3323629dda9f31c35e81cb06a602cf
Kevin Bargary <kevinbargary@gmail.com>
2023-04-05 18:22:54 +0000

    Update 43 files

--- src/App.js
+++ src/App.js
@@ -1,46 +1,120 @@
-import React, { Component } from "react";
-import logo from "./Logo.png";
-import bookkeeping from "./bookkeeping.jpg";
-import vat from "./vat.jpg";
-import creditcontrol from "./creditcontrol.jpg";
-import payroll from "./payroll.jpg";
-import tax from "./tax.jpg";
+import React, { useEffect } from "react";
+import logo from "./images/Logo.png";
+import bookkeeping from "./images/bookkeeping.jpg";
+import vat from "./images/vat.jpg";
+import creditcontrol from "./images/creditcontrol.jpg";
+import payroll from "./images/payroll.jpg";
+import tax from "./images/tax.jpg";
+import ContactForm from "./ContactForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
-import "./App.css";
+import "./styles/App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <header>
-          <img className="logo" src={logo} alt="" />
+function LoadEvents() {
+  const header = document.querySelector("header");
 
-          <nav>
-            <ul>
-              <li>
-                <a href="#home" class="active">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#services">Services</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main>
-          <section class="section1">
-            <div class="content">
-              <p>Keeping your books balanced and stress-free.</p>
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+}
+
+function App() {
+  useEffect(() => LoadEvents(), []);
+  return (
+    <>
+      <header>
+        <img className="logo" src={logo} alt="" />
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#home" class="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section class="section1" id="home">
+          <div class="content">
+            <p>Keeping your books balanced and stress-free.</p>
+          </div>
+        </section>
+        <section class="about">
+          <div class="container">
+            <h2>About</h2>
+            <p>
+              NB Bookkeeping was founded in Tipperary in 2023, and is run by
+              Nicola Bargary, a professionally qualified Bookkeeper, proficient
+              in multiple accounting softwares such as Sage, Surf Accounts,
+              Quickbooks and Xero. With 10+ years experience working both in the
+              UK and Ireland in an accounts department, I have had experience
+              working in all aspects of Accounting, from AP/AR to the
+              preparation of Management Accounts, working across a variety of
+              sectors including Healthcare, E-Commerce, Haulage, Construction,
+              and Manufacturing. As a small operation, I can provide a
+              personalised service, tailored to your business needs. I am quick,
+              efficient and accurate, allowing you to have an up to date
+              snapshot on the financial health of your business. By providing
+              you with a virtual accounts department, I can take the stress out
+              of paperwork, giving you peace of mind, and enabling you to focus
+              on growing your business.
+            </p>
+            <div class="infographics">
+              <div class="infographic">
+                <img src="graphic-1.png" alt="Graphic 1" />
+                <h3>Stat 1</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  euismod, tortor non convallis eleifend, eros ante mattis
+                  augue, non porta neque massa ut lorem.
+                </p>
+              </div>
+              <div class="infographic">
+                <img src="graphic-2.png" alt="Graphic 2" />
+                <h3>Stat 2</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  euismod, tortor non convallis eleifend, eros ante mattis
+                  augue, non porta neque massa ut lorem.
+                </p>
+              </div>
+              <div class="infographic">
+                <img src="graphic-3.png" alt="Graphic 3" />
+                <h3>Stat 3</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  euismod, tortor non convallis eleifend, eros ante mattis
+                  augue, non porta neque massa ut lorem.
+                </p>
+              </div>
             </div>
-          </section>
-          <section class="section2">
+          </div>
+        </section>
+
+        <section class="section2" id="services">
+          <div class="container">
+            <h2>Services Provided</h2>
             <div class="content">
               <div class="card-grid">
                 <div class="card">
@@ -122,35 +196,32 @@ class App extends Component {
                 </div>
               </div>
             </div>
-          </section>
-          <section class="section3">
-            <div class="content">
-              <p>
-                <form id="contact-form" method="post" action="submit-form.php">
-                  <label for="name">Name</label>
-                  <input type="text" id="name" name="name" required />
+          </div>
+        </section>
+        <section id="contact" class="section3">
+          <ContactForm />
+        </section>
+      </main>
+      <footer>
+        <p>Copyright NB Bookkeeping © 2023</p>
 
-                  <label for="email">Email</label>
-                  <input type="email" id="email" name="email" required />
-
-                  <label for="subject">Subject</label>
-                  <input type="text" id="subject" name="subject" required />
-
-                  <label for="message">Message</label>
-                  <textarea id="message" name="message" required></textarea>
-
-                  <button type="submit">Send</button>
-                </form>
-              </p>
-            </div>
-          </section>
-        </main>
-        <footer>
-          <p>Copyright NB Bookkeeping © 2023</p>
-        </footer>
-      </>
-    );
-  }
+        <div className="social-media">
+          <a href="https://www.facebook.com">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a href="https://twitter.com">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="https://www.instagram.com">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://www.linkedin.com">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default App;
